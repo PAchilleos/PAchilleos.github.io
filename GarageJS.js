9f6a2e255fb79671802000d33a214338dbb31897
PAchilleos <procopis.achilleos@qa.com>
2017-08-24 10:12:26 +0100

    added price bracket

--- GarageJS.js
+++ GarageJS.js
@@ -5,31 +5,40 @@ function addcar() {
         Make: document.getElementById("make").value,
         Model: document.getElementById("model").value,
         year: document.getElementById("year").value,
-        reg: document.getElementById("reg").value
+        reg: document.getElementById("reg").value,
+        prc: ""
     };
 
     CarList.push(car);
     console.log(CarList);
+    var e = document.getElementById("prc");
+    var strUser = e.options[e.selectedIndex].text;
+    car.prc = strUser;
 }
 
 function cardeets() {
     var container = document.getElementById("deets");
     container.innerHTML = "";
     let val = document.getElementById("deet").value;
-    //console.log(document.getElementById("pltdeet").value)
+
     var a = CarList.filter((b) => {
         return b.reg === (val) || b.Make === (val) || b.Model === (val) || b.year === (val)
     });
     console.log(a);
     if (a.length !== 0) {
         for (let i = 0; i < a.length; i++) {
+            if (a[i].prc.includes("Bracket")) {
+                a[i].prc = "N/A";
+            }
             var par = document.createElement("p");
-            par.innerHTML = "Make: " + a[i].Make + "<br> Model: " + a[i].Model + "<br> Year: " + a[i].year + "<br> RegNo: " + a[i].reg;
+            par.innerHTML = "Make: " + a[i].Make + "<br> Model: " + a[i].Model + "<br> Year: " + a[i].year + "<br> RegNo: " + a[i].reg + "<br> Price: " + a[i].prc;
             container.appendChild(par);
         }
     } else {
         container.innerHTML = "Car does not exist in the Garage";
     }
+
+
 
 }
 
@@ -39,7 +48,7 @@ function remcar() {
         return b.reg === (document.getElementById("pltrem").value);
     });
 
-    container.innerHTML = "The " + CarList[index].Make + "with the registration number of  " + CarList[0].reg + " has been removed";
+    container.innerHTML = "The " + CarList[index].Make + " with the registration number of  " + CarList[0].reg + " has been removed";
     console.log(index);
     CarList.splice(index, 1);
     console.log(CarList);
